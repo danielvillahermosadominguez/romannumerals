507199123f2d0a392812a2b1b8579904d5133485
Daniel <daniel.villahermosa@codurance.com>
2022-11-22 15:46:49 +0100

    ready to start

---
+++ romanumerals.tests/ExampleShould.cs
@@ -0,0 +1,17 @@
+namespace romanumerals.tests
+{
+    [TestFixture]
+    public class ExampleShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Doing_nothing()
+        {
+            Assert.Pass();
+        }
+    }
+}
