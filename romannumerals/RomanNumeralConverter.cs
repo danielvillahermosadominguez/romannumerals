6e375a1f6c09af9d73bbfdc4be0c32dcd65637a0
Daniel <daniel.villahermosa@codurance.com>
2022-11-23 17:23:43 +0100

    Until the number 11

--- romannumerals/RomanNumeralConverter.cs
+++ romannumerals/RomanNumeralConverter.cs
@@ -2,15 +2,58 @@ namespace romanumerals;
 
 public class RomanNumeralConverter
 {
+    private const int MAX_RANGE = 3000;
+    private const int MIN_RANGE = 1;
     private int _number;
+    private Dictionary<int, string> _units;
 
     public RomanNumeralConverter(int number)
     {
         _number = number;
+        initializeUnits();
     }
 
-    public void Convert()
+    public string Convert()
     {
-        throw new OutOfRange();
+        checkOutOfRange();
+
+        if (_units.ContainsKey(_number))
+        {
+            return _units[_number];
+        }
+
+        if (_number > 10)
+        {
+            return concatISymbols(10);
+        }
+
+        return concatISymbols(5);
+    }
+
+    private void checkOutOfRange()
+    {
+        if (_number < MIN_RANGE || _number > MAX_RANGE)
+        {
+            throw new OutOfRange();
+        }
+    }
+
+    private string concatISymbols(int number)
+    {
+        return _units[number] + _units[_number % 10];
+    }
+
+    private void initializeUnits()
+    {
+        _units = new Dictionary<int, string>()
+        {
+            { 1, "I"},
+            { 2, "II" },
+            { 3, "III" },
+            { 4, "IV" },
+            { 5, "V" },
+            { 9, "IX"},
+            { 10, "X"}
+        };
     }
 }
--- romanumerals.tests/ExampleShould.cs
+++ romanumerals.tests/ExampleShould.cs
@@ -3,12 +3,36 @@ namespace romanumerals.tests
     [TestFixture]
     public class RomanNumeralConverterShould
     {
-        [Test]
-        public void Throw_exception_when_the_input_number_is_zero()
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(3001)]
+        public void Throw_exception_when_the_input_number_is_out_of_range(int number)
         {
-            var romanNumeralConverter = new RomanNumeralConverter(0);
+            var romanNumeralConverter = new RomanNumeralConverter(number);
 
             Assert.Throws<OutOfRange>(() => romanNumeralConverter.Convert());
+        }
+
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        public void Convert_the_number_to_a_roman_text(
+            int number,
+            string roman)
+        {
+            var romanNumeralConverter = new RomanNumeralConverter(number);
+
+            var result = romanNumeralConverter.Convert();
+
+            Assert.That(result, Is.EqualTo(roman));
         }
     }
 }
